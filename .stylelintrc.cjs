fa966cad6b08f680c8785367c4beec7804db7b3c
teleskop150750 <teleskop150750gmail.com>
2022-04-16 17:31:13 +0300

    selector-class-pattern

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@teleskop150750/stylelint-config-vue-prettier'],
   rules: {
+    'selector-class-pattern': '.*',
     // 'selector-pseudo-element-no-unknown': false,
   },
 };
